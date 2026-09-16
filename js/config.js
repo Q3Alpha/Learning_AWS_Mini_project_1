b1d993d3518c2e84ecbd42d770d583fa834c4136
S.A.Johan <connect.syedasifjohan@gmail.com>
2026-09-16 19:34:38 +0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_NKNp8QpBQ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5egcrd3me2fadigum4mlob71k6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '2j2inef4qcuafd55v0kjku1o0d', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
